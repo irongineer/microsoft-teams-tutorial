1a7a1f95d3c635d12d01a7c81d45fa7bcc96ab2e
irongineer <daichi.hotchi@gmail.com>
2021-03-15 17:27:52 +0900

    Creating conversational bots for Microsoft Teams

--- my-teams-bot001/src/server/conversationalBot/ConversationalBot.ts
+++ my-teams-bot001/src/server/conversationalBot/ConversationalBot.ts
@@ -1,10 +1,11 @@
 import { BotDeclaration } from "express-msteams-host";
 import * as debug from "debug";
 import { DialogSet, DialogState } from "botbuilder-dialogs";
-import { StatePropertyAccessor, CardFactory, TurnContext, MemoryStorage, ConversationState, ActivityTypes, TeamsActivityHandler } from "botbuilder";
+import { StatePropertyAccessor, CardFactory, TurnContext, MemoryStorage, ConversationState, ActivityTypes, TeamsActivityHandler, MessageFactory } from "botbuilder";
 import HelpDialog from "./dialogs/HelpDialog";
 import WelcomeCard from "./dialogs/WelcomeDialog";
-
+import * as Util from "util";
+const TextEncorder = Util.TextEncoder;
 // Initialize debug logging module
 const log = debug("msteams");
 
@@ -43,7 +44,10 @@ export class ConversationalBot extends TeamsActivityHandler {
                     {
                         let text = TurnContext.removeRecipientMention(context.activity);
                         text = text.toLowerCase();
-                        if (text.startsWith("hello")) {
+                        if (text.startsWith("mentionme")) {
+                            await this.handleMessageMentionMeOneOnOne(context);
+                            return;
+                        } else if (text.startsWith("hello")) {
                             await context.sendActivity("Oh, hello to you as well!");
                             return;
                         } else if (text.startsWith("help")) {
@@ -81,6 +85,17 @@ export class ConversationalBot extends TeamsActivityHandler {
                 });
             }
         });
-   }
+    }
 
+    private async handleMessageMentionMeOneOnOne(context: TurnContext): Promise<void> {
+        const mention = {
+            mentioned: context.activity.from,
+            text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
+            type: "mention"
+        };
+
+        const replyActivity = MessageFactory.text(`Hi ${mention.text} from a 1:1 chat.`);
+        replyActivity.entities = [mention];
+        await context.sendActivity(replyActivity);
+    }
 }
